331ac45b0df6657a43f90b2b37425d80f814cbab
rshd <rachidmokhlis102@gmail.com>
2026-04-19 15:19:15 +0200

    Change server port from 5000 to 3000

--- server/index.js
+++ server/index.js
@@ -42,5 +42,5 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB error:', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
